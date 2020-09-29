fecc55edb4cfc835f017b568f26b352f2e038409
CarmenLuisa <69864949+CarmenLuisa@users.noreply.github.com>
2020-09-29 14:48:45 +0100

    Add files via upload

---
+++ Catherine_Carmen_Andrea/Final version group project/js/theDOM.js
@@ -0,0 +1,252 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //first question
+    var wrongButtonA = document.getElementById("wrongAnswer1");
+    wrongButtonA.addEventListener('click', function () {
+        wrongButtonA.style.backgroundColor = "red";
+        wrongButtonA.style.color = "white";
+        wrongButtonA.innerHTML = "Try again :)";
+    })
+    var wrongButtonB = document.getElementById("wrongAnswer2");
+    wrongButtonB.addEventListener('click', function () {
+        wrongButtonB.style.backgroundColor = "red";
+        wrongButtonB.style.color = "white";
+        wrongButtonB.innerHTML = "Try again :)";
+    })
+    var rightButtonA = document.getElementById("rightAnswer1");
+    rightButtonA.addEventListener('click', function () {
+        rightButtonA.style.backgroundColor = "green";
+        rightButtonA.style.color = "white";
+        rightButtonA.innerHTML = "Correct!";
+    })
+    var wrongButtonC = document.getElementById("wrongAnswer3");
+    wrongButtonC.addEventListener('click', function () {
+        wrongButtonC.style.backgroundColor = "red";
+        wrongButtonC.style.color = "white";
+        wrongButtonC.innerHTML = "Try again :)";
+    })
+    //second question
+    var rightButtonB = document.getElementById("rightAnswer2");
+    rightButtonB.addEventListener('click', function () {
+        rightButtonB.style.backgroundColor = "green";
+        rightButtonB.style.color = "white";
+        rightButtonB.innerHTML = "Correct!";
+    })
+    var wrongButtonD = document.getElementById("wrongAnswer4");
+    wrongButtonD.addEventListener('click', function () {
+        wrongButtonD.style.backgroundColor = "red";
+        wrongButtonD.style.color = "white";
+        wrongButtonD.innerHTML = "Try again :)";
+    })
+    var wrongButtonE = document.getElementById("wrongAnswer5");
+    wrongButtonE.addEventListener('click', function () {
+        wrongButtonE.style.backgroundColor = "red";
+        wrongButtonE.style.color = "white";
+        wrongButtonE.innerHTML = "Try again :)";
+    })
+    var wrongButtonF = document.getElementById("wrongAnswer6");
+    wrongButtonF.addEventListener('click', function () {
+        wrongButtonF.style.backgroundColor = "red";
+        wrongButtonF.style.color = "white";
+        wrongButtonF.innerHTML = "Try again :)";
+    })
+    //third question
+    var wrongButtonG = document.getElementById("wrongAnswer7");
+    wrongButtonG.addEventListener('click', function () {
+        wrongButtonG.style.backgroundColor = "red";
+        wrongButtonG.style.color = "white";
+        wrongButtonG.innerHTML = "Try again :)";
+    })
+    var rightButtonC = document.getElementById("rightAnswer3");
+    rightButtonC.addEventListener('click', function () {
+        rightButtonC.style.backgroundColor = "green";
+        rightButtonC.style.color = "white";
+        rightButtonC.innerHTML = "Correct!";
+    })
+    var wrongButtonH = document.getElementById("wrongAnswer8");
+    wrongButtonH.addEventListener('click', function () {
+        wrongButtonH.style.backgroundColor = "red";
+        wrongButtonH.style.color = "white";
+        wrongButtonH.innerHTML = "Try again :)";
+    })
+    var wrongButtonI = document.getElementById("wrongAnswer9");
+    wrongButtonI.addEventListener('click', function () {
+        wrongButtonI.style.backgroundColor = "red";
+        wrongButtonI.style.color = "white";
+        wrongButtonI.innerHTML = "Try again :)";
+    })
+    //fourth question
+    var wrongButtonJ = document.getElementById("wrongAnswer10");
+    wrongButtonJ.addEventListener('click', function () {
+        wrongButtonJ.style.backgroundColor = "red";
+        wrongButtonJ.style.color = "white";
+        wrongButtonJ.innerHTML = "Try again :)";
+    })
+    var wrongButtonK = document.getElementById("wrongAnswer11");
+    wrongButtonK.addEventListener('click', function () {
+        wrongButtonK.style.backgroundColor = "red";
+        wrongButtonK.style.color = "white";
+        wrongButtonK.innerHTML = "Try again :)";
+    })
+    var rightButtonD = document.getElementById("rightAnswer4");
+    rightButtonD.addEventListener('click', function () {
+        rightButtonD.style.backgroundColor = "green";
+        rightButtonD.style.color = "white";
+        rightButtonD.innerHTML = "Correct!";
+    })
+    var wrongButtonL = document.getElementById("wrongAnswer12");
+    wrongButtonL.addEventListener('click', function () {
+        wrongButtonL.style.backgroundColor = "red";
+        wrongButtonL.style.color = "white";
+        wrongButtonL.innerHTML = "Try again :)";
+    })
+    //fifth question
+    var wrongButtonM = document.getElementById("wrongAnswer13");
+    wrongButtonM.addEventListener('click', function () {
+        wrongButtonM.style.backgroundColor = "red";
+        wrongButtonM.style.color = "white";
+        wrongButtonM.innerHTML = "Try again :)";
+    })
+    var wrongButtonN = document.getElementById("wrongAnswer14");
+    wrongButtonN.addEventListener('click', function () {
+        wrongButtonN.style.backgroundColor = "red";
+        wrongButtonN.style.color = "white";
+        wrongButtonN.innerHTML = "Try again :)";
+    })
+    var wrongButtonO = document.getElementById("wrongAnswer15");
+    wrongButtonO.addEventListener('click', function () {
+        wrongButtonO.style.backgroundColor = "red";
+        wrongButtonO.style.color = "white";
+        wrongButtonO.innerHTML = "Try again :)";
+    })
+    var rightButtonE = document.getElementById("rightAnswer5");
+    rightButtonE.addEventListener('click', function () {
+        rightButtonE.style.backgroundColor = "green";
+        rightButtonE.style.color = "white";
+        rightButtonE.innerHTML = "Correct!";
+    })
+    //sixth question
+    var wrongButtonP = document.getElementById("wrongAnswer16");
+    wrongButtonP.addEventListener('click', function () {
+        wrongButtonP.style.backgroundColor = "red";
+        wrongButtonP.style.color = "white";
+        wrongButtonP.innerHTML = "Try again :)";
+    })
+    var wrongButtonQ = document.getElementById("wrongAnswer17");
+    wrongButtonQ.addEventListener('click', function () {
+        wrongButtonQ.style.backgroundColor = "red";
+        wrongButtonQ.style.color = "white";
+        wrongButtonQ.innerHTML = "Try again :)";
+    })
+    var wrongButtonR = document.getElementById("wrongAnswer18");
+    wrongButtonR.addEventListener('click', function () {
+        wrongButtonR.style.backgroundColor = "red";
+        wrongButtonR.style.color = "white";
+        wrongButtonR.innerHTML = "Try again :)";
+    })
+    var rightButtonF = document.getElementById("rightAnswer6");
+    rightButtonF.addEventListener('click', function () {
+        rightButtonF.style.backgroundColor = "green";
+        rightButtonF.style.color = "white";
+        rightButtonF.innerHTML = "Correct!";
+    })
+    //seventh question
+    var wrongButtonS = document.getElementById("wrongAnswer19");
+    wrongButtonS.addEventListener('click', function () {
+        wrongButtonS.style.backgroundColor = "red";
+        wrongButtonS.style.color = "white";
+        wrongButtonS.innerHTML = "Try again :)";
+    })
+    var rightButtonG = document.getElementById("rightAnswer7");
+    rightButtonG.addEventListener('click', function () {
+        rightButtonG.style.backgroundColor = "green";
+        rightButtonG.style.color = "white";
+        rightButtonG.innerHTML = "Correct!";
+    })
+    var wrongButtonT = document.getElementById("wrongAnswer20");
+    wrongButtonT.addEventListener('click', function () {
+        wrongButtonT.style.backgroundColor = "red";
+        wrongButtonT.style.color = "white";
+        wrongButtonT.innerHTML = "Try again :)";
+    })
+    var wrongButtonU = document.getElementById("wrongAnswer21");
+    wrongButtonU.addEventListener('click', function () {
+        wrongButtonU.style.backgroundColor = "red";
+        wrongButtonU.style.color = "white";
+        wrongButtonU.innerHTML = "Try again :)";
+    })
+    //eight question
+    var wrongButtonV = document.getElementById("wrongAnswer22");
+    wrongButtonV.addEventListener('click', function () {
+        wrongButtonV.style.backgroundColor = "red";
+        wrongButtonV.style.color = "white";
+        wrongButtonV.innerHTML = "Try again :)";
+    })
+    var wrongButtonW = document.getElementById("wrongAnswer23");
+    wrongButtonW.addEventListener('click', function () {
+        wrongButtonW.style.backgroundColor = "red";
+        wrongButtonW.style.color = "white";
+        wrongButtonW.innerHTML = "Try again :)";
+    })
+    var wrongButtonX = document.getElementById("wrongAnswer24");
+    wrongButtonX.addEventListener('click', function () {
+        wrongButtonX.style.backgroundColor = "red";
+        wrongButtonX.style.color = "white";
+        wrongButtonX.innerHTML = "Try again :)";
+    })
+    var rightButtonH = document.getElementById("rightAnswer8");
+    rightButtonH.addEventListener('click', function () {
+        rightButtonH.style.backgroundColor = "green";
+        rightButtonH.style.color = "white";
+        rightButtonH.innerHTML = "Correct!";
+    })
+    //ninth question
+    var rightButtonI = document.getElementById("rightAnswer9");
+    rightButtonI.addEventListener('click', function () {
+        rightButtonI.style.backgroundColor = "green";
+        rightButtonI.style.color = "white";
+        rightButtonI.innerHTML = "Correct!";
+    })
+    var wrongButtonY = document.getElementById("wrongAnswer25");
+    wrongButtonY.addEventListener('click', function () {
+        wrongButtonY.style.backgroundColor = "red";
+        wrongButtonY.style.color = "white";
+        wrongButtonY.innerHTML = "Try again :)";
+    })
+    var wrongButtonZ = document.getElementById("wrongAnswer26");
+    wrongButtonZ.addEventListener('click', function () {
+        wrongButtonZ.style.backgroundColor = "red";
+        wrongButtonZ.style.color = "white";
+        wrongButtonZ.innerHTML = "Try again :)";
+    })
+    var wrongButtonAA = document.getElementById("wrongAnswer27");
+    wrongButtonAA.addEventListener('click', function () {
+        wrongButtonAA.style.backgroundColor = "red";
+        wrongButtonAA.style.color = "white";
+        wrongButtonAA.innerHTML = "Try again :)";
+    })
+    //tenth question
+    var wrongButtonAB = document.getElementById("wrongAnswer28");
+    wrongButtonAB.addEventListener('click', function () {
+        wrongButtonAB.style.backgroundColor = "red";
+        wrongButtonAB.style.color = "white";
+        wrongButtonAB.innerHTML = "Try again :)";
+    })
+    var rightButtonJ = document.getElementById("rightAnswer10");
+    rightButtonJ.addEventListener('click', function () {
+        rightButtonJ.style.backgroundColor = "green";
+        rightButtonJ.style.color = "white";
+        rightButtonJ.innerHTML = "Correct!";
+    })
+    var wrongButtonAC = document.getElementById("wrongAnswer29");
+    wrongButtonAC.addEventListener('click', function () {
+        wrongButtonAC.style.backgroundColor = "red";
+        wrongButtonAC.style.color = "white";
+        wrongButtonAC.innerHTML = "Try again :)";
+    })
+    var wrongButtonAD = document.getElementById("wrongAnswer30");
+    wrongButtonAD.addEventListener('click', function () {
+        wrongButtonAD.style.backgroundColor = "red";
+        wrongButtonAD.style.color = "white";
+        wrongButtonAD.innerHTML = "Try again :)";
+    })
+});
